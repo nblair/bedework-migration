0e5e58829eb7199c1f95d9ce7d417bfc93ce3e12
Eric Wittmann <eric.wittmann@redhat.com>
2012-11-28 14:05:47 -0500

    some cleanup of the bw-ws poms

--- migration-scripts/bw-ws.sh
+++ migration-scripts/bw-ws.sh
@@ -34,7 +34,9 @@ rm -rf icalendar
 rm -rf icalendar-strict
 rm -rf timezones
 rm -rf vcard
+
 # Reorganize calws-soap
+# ###############################3
 mkdir -p src/main/wsdls/calws-soap
 mkdir -p src/main/schemas/calws-soap
 git mv calws-soap/*.xsd src/main/schemas/calws-soap/.
@@ -44,4 +46,31 @@ git rm -r calws-soap
 rm -rf calws-soap
 sed -i 's/\.\.\/schemas\//\.\.\//' src/main/schemas/calws-soap/*.xsd
 sed -i 's/wsmessages\.xsd/\.\.\/\.\.\/schemas\/calws-soap\/wsmessages\.xsd/' src/main/wsdls/calws-soap/*.wsdl
+
+# Reorganize calws-soap-strict
+# ###############################3
+mkdir -p src/main/wsdls/calws-soap-strict
+mkdir -p src/main/schemas/calws-soap-strict
+git mv calws-soap-strict/*.xsd src/main/schemas/calws-soap-strict/.
+git mv calws-soap-strict/wsdlbindings src/main/wsdls/calws-soap-strict/.
+git mv calws-soap-strict/*.wsdl src/main/wsdls/calws-soap-strict/.
+git rm -r calws-soap-strict
+rm -rf calws-soap-strict
+sed -i 's/\.\.\/schemas\//\.\.\//' src/main/schemas/calws-soap-strict/*.xsd
+sed -i 's/wsmessages\.xsd/\.\.\/\.\.\/schemas\/calws-soap-strict\/wsmessages\.xsd/' src/main/wsdls/calws-soap-strict/*.wsdl
+
+# Reorganize exchangews
+# ###############################3
+mkdir -p src/main/wsdls/exchangews
+mkdir -p src/main/schemas/exchangews
+git mv exchangews/*.xsd src/main/schemas/exchangews/.
+git mv exchangews/wsdlbindings src/main/wsdls/exchangews/.
+git mv exchangews/*.wsdl src/main/wsdls/exchangews/.
+git rm -r exchangews
+rm -rf exchangews
+sed -i 's/\.\.\/schemas\//\.\.\//' src/main/schemas/exchangews/*.xsd
+sed -i 's/messages\.xsd/\.\.\/\.\.\/schemas\/exchangews\/messages\.xsd/' src/main/wsdls/exchangews/*.wsdl
+
+
+
 
